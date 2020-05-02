bf81e085156369347333a0072cb72cbfd4be8781
someone in the universe <63373211+SomeoneInTheUniverse@users.noreply.github.com>
2020-05-02 17:30:32 +1000

    Create package.js

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+  "name": "Axis Presents",
+  "version": "0.01",
+  "description": "Give kids presents!",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [],
+  "author": "Joshiy08#1772",
+  "license": "ISC"
+}
